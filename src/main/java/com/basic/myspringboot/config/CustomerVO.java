41fe1437f8a68a59b2098d7f15476d320b9daa9c
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-30 14:36:34 +0900

    config Test 작성

--- src/main/java/com/basic/myspringboot/config/CustomerVO.java
+++ src/main/java/com/basic/myspringboot/config/CustomerVO.java
@@ -1,5 +1,12 @@
 package com.basic.myspringboot.config;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
 public class CustomerVO {
     private String mode;
     private double rate;
---
+++ src/main/java/com/basic/myspringboot/config/TestConfig.java
@@ -0,0 +1,15 @@
+package com.basic.myspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+    @Bean
+    public CustomerVO customerVO() {
+        return CustomerVO.builder() //CustomerVOBuilder
+                .mode("테스트환경")
+                .rate(0.5)
+                .build();
+    }
+}
