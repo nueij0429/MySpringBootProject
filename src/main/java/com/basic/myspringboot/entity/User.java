5c3581614464eb11ba9c1a8340dd18cb9b50bd78
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-09 10:30:26 +0900

    @NotBlank, @NotEmpty 어노테이션 추가

--- src/main/java/com/basic/myspringboot/entity/User.java
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -1,6 +1,8 @@
 package com.basic.myspringboot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,9 +20,11 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    @NotEmpty(message = "Name은 필수 입력 항목입니다.")
     private String name;
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email은 필수 입력 항목입니다.")
     private String email;
 
     @Column(nullable = false, updatable = false)
